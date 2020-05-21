58ada7f7168e10b6b08ac223f429f1e313c5d3c4
lyna <vuthilyna304@gmail.com>
2020-05-21 14:22:23 +0700

    all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap-4-react';
+import { Modal, Button } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        show: false
+      };
+ 
+
+  handleModal = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    
+    return (
+      
+      <div>
+      
+        <Button onClick={this.handleModal}>open modal</Button>
+        <Modal show={this.state.show}>
+          <Modal.Header>Header</Modal.Header>
+          <Modal.Body><p>Lorem ipsum dolor sit amet consectetur
+     adipisicing elit. Ullam, architecto eos! Possimus molliti
+      laudantium deleniti, nulla illo esse excepturi error volup
+      tatibus labore quibusdam beatae nisi dolor maxime adipisci 
+      nemo delectus!</p></Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleModal}>close modal</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Bodymodal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Bodymodal extends Component {
+  render() {
+    return (
+      <div>
+       <p>Lorem ipsum dolor sit amet consectetur
+     adipisicing elit. Ullam, architecto eos! Possimus molliti
+      laudantium deleniti, nulla illo esse excepturi error volup
+      tatibus labore quibusdam beatae nisi dolor maxime adipisci 
+      nemo delectus!</p>
+      </div>
+    );
+  }
+}
+
+export default Bodymodal;
